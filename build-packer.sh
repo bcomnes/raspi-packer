f316ab7de797f805132682d515fa3b61b4c279b9
Bret Comnes <bcomnes@gmail.com>
2019-01-06 20:51:46 -0800

    Try a build

---
+++ build-packer.sh
@@ -0,0 +1,5 @@
+go get github.com/hashicorp/packer
+go build -o packer github.com/hashicorp/packer
+
+go get github.com/solo-io/packer-builder-arm-image
+go build -o packer-builder-arm-image github.com/solo-io/packer-builder-arm-image
--- build-plugin.sh
+++
@@ -1,5 +0,0 @@
-cd packer-builder-arm-image
-
-go mod download
-go build
-mv packer-builder-arm-image ..
