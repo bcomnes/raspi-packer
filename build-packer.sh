f3aa95e2f78b9376f5bc1c87c6a96e53a6ac9e54
Bret Comnes <bcomnes@gmail.com>
2019-01-07 07:57:31 -0800

    Try using sudo

--- build-packer.sh
+++ build-packer.sh
@@ -1,5 +1,6 @@
 go get github.com/hashicorp/packer
 go build -o packer github.com/hashicorp/packer
+./packer version
 
 go get github.com/solo-io/packer-builder-arm-image
 go build -o packer-builder-arm-image github.com/solo-io/packer-builder-arm-image
