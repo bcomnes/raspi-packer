8cbeaac4ce82d4ec43b4207619555dff048cf598
Bret Comnes <bcomnes@gmail.com>
2019-01-28 13:22:11 -0800

    Add missing dev

--- build-packer.sh
+++ build-packer.sh
@@ -1,6 +1,10 @@
+echo "Building packer"
 go get github.com/hashicorp/packer
 go build -o packer github.com/hashicorp/packer
 ./packer version
+echo "Finished building packer"
 
+echo "Building solo-io/packer-builder-arm-image"
 go get github.com/solo-io/packer-builder-arm-image
 go build -o packer-builder-arm-image github.com/solo-io/packer-builder-arm-image
+echo "Finished building solo-io/packer-builder-arm-image"
