c54e3ab606c0e97949ceda31f2909a475f5c346f
Bret Comnes <bcomnes@gmail.com>
2020-03-30 16:31:35 -0600

    start build-packer

--- build-packer.sh
+++ build-packer.sh
@@ -1,6 +1,13 @@
-echo "Building packer"
-go get -v -u github.com/hashicorp/packer
-go build -v -o packer github.com/hashicorp/packer
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+PACKER_VERSION="$1"
+
+echo "Downloading packer"
+wget -q "https://releases.hashicorp.com/packer/1.5.5/packer_${PACKER_VERSION}_linux_amd64.zip";
+unzip "packer_${PACKER_VERSION}_linux_amd64.zip"
 ./packer version
 echo "Finished building packer"
 
