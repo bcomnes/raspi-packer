a56ee1bebe2864c6ac3cff0d50c5e91accae77a7
Bret Comnes <bcomnes@gmail.com>
2020-04-01 16:58:53 -0600

    try new builder

--- build-packer.sh
+++ build-packer.sh
@@ -4,19 +4,20 @@ set -e
 set -x
 
 PACKER_VERSION="$1"
-ARM_BUILDER_VERSION="$2"
 
 echo "Downloading packer"
-wget -q "https://releases.hashicorp.com/packer/1.5.5/packer_${PACKER_VERSION}_linux_amd64.zip";
+wget -q "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip";
 unzip "packer_${PACKER_VERSION}_linux_amd64.zip"
 ./packer version
 echo "Finished building packer"
 
-echo "downloading solo-io/packer-builder-arm-image"
-wget -q "https://github.com/solo-io/packer-builder-arm-image/releases/download/v${ARM_BUILDER_VERSION}/packer-builder-arm-image"
-wget -q "https://github.com/solo-io/packer-builder-arm-image/releases/download/v${ARM_BUILDER_VERSION}/flasher"
-chmod +x ./packer-builder-arm-image
-chmod +x ./flasher
+# echo "Building packer"
+# go build -v -o packer github.com/hashicorp/packer
+# ./packer version
+# echo "Finished building packer"
+
+echo "Building github.com/mkaczanowski/packer-builder-arm"
+go build -v -o packer-builder-arm github.com/mkaczanowski/packer-builder-arm
 echo "Finished building solo-io/packer-builder-arm-image"
 
 ls -la
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bcomnes/raspi-packer
+
+go 1.14
+
+require (
+	github.com/hashicorp/packer v1.5.5
+	github.com/mkaczanowski/packer-builder-arm v0.0.0-20200330001141-c143fd9ada0c
+)
