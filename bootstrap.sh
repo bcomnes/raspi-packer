580cb72057ba002c342d04d247ebcf3a0fa73890
Bret Comnes <bcomnes@gmail.com>
2020-04-03 15:27:26 -0600

    small cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,23 @@
+#!/usr/bin/env bash
+
 # bret-mbr:sbc-bootstrap bret$ ssh bret@192.168.1.12
 set -e
 set -x
 
 # Recomended in https://wiki.archlinux.org/index.php/Chroot#Using_chroot
 source /etc/profile
+# Debug info
+env
 
 # First boot install step: https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-3
 pacman-key --init
 pacman-key --populate archlinuxarm
 
+# Enable ntp
 # TODO: can you do this with text?
-# timedatectl set-ntp true
+#timedatectl set-ntp true
 
 # Enable network connection
-## TODO double check this is restored on boot
 mv /etc/resolv.conf /etc/resolv.conf.bk
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 pacman -Sy --noconfirm --needed
@@ -26,41 +30,29 @@ echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 # Etckeeper init
 pacman -S git etckeeper --noconfirm --needed
 
-env
-
 export HOME=/root
-# TODO: This doesn't work in chroot
 git config --global user.email "bcomnes@gmail.com"
 git config --global user.name "Bret Comnes"
-#
-# This is a guess workaround
-#cat > /root/.gitconfig <<- EOM
-#[user]
-#  email = bcomnes@gmail.com
-#  name = Bret Comnes
-#EOM
 
 etckeeper init
 
-# Doesn't work
-#cd /etc
-#git add -A
-#git commit -m 'initial commit'
+cd /etc
+git add -A
+git commit -m 'initial commit'
 
 systemctl enable etckeeper.timer
 systemctl start etckeeper.timer
 
 # Set Hostname
-# TODO: Usually do this, but it doesn't work
+# Normally we use hostnamectl, but that doesn't work in chroot
 #hostnamectl set-hostname raspi3
-# This is a guess workaround
 echo raspi3 > /etc/hostname
 
 # Install avahi and stuff
 # TODO: Figure out if systemd has this built in now
 pacman -S vim htop sudo avahi nss-mdns --noconfirm --needed
 
-# Set up sudo
+# Set up no-password sudo
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
 
 # Set up avahi
