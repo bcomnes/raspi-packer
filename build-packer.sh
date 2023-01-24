24508161f46f6d137e84b23f45bded72f61dea2c
Bret Comnes <bcomnes@gmail.com>
2023-01-24 10:50:14 -0700

    Cleanup

--- build-packer.sh
+++ build-packer.sh
@@ -5,11 +5,11 @@ set -x
 
 PACKER_VERSION="$1"
 
-echo "Downloading packer"
-wget -q "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip";
-unzip "packer_${PACKER_VERSION}_linux_amd64.zip"
-#./packer version
-echo "Finished building packer"
+# echo "Downloading packer"
+# wget -q "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip";
+# unzip "packer_${PACKER_VERSION}_linux_amd64.zip"
+# #./packer version
+# echo "Finished building packer"
 
 # Src build of packer
 #echo "Building packer"
